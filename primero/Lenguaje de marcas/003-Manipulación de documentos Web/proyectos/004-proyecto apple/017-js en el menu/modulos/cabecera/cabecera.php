20d85e186123d39f58455521e810142a906402c7
Francisco Jose <franhr1113@gmail.com>
2024-11-29 00:51:12 +0100

    1

---
+++ primero/Lenguaje de marcas/003-Manipulación de documentos Web/proyectos/004-proyecto apple/017-js en el menu/modulos/cabecera/cabecera.php
@@ -0,0 +1,17 @@
+<style>
+<?php include 'cabecera.css'; ?>
+</style>
+<header>
+    <div class="contenedor">
+        <img src="img/logo.svg">
+            <nav>
+                <ul>
+                    
+
+                </ul>
+            </nav>
+    </div>
+</header>
+<script>
+<?php include 'cabecera.js'; ?>
+</script>
