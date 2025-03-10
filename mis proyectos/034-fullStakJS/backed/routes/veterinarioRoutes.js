d3a4b738d28581ab804633d6249234d0c2a26295
Francisco Jose <franhr1113@gmail.com>
2025-03-10 16:49:50 +0100

    v1

---
+++ mis proyectos/034-fullStakJS/backed/routes/veterinarioRoutes.js
@@ -0,0 +1,19 @@
+import express from 'express'
+const router = express.Router()
+import {registrar, perfil, confirmar, autenticar, olvidePassword,comprobarToken,nuevoPassword} from '../controllers/veterinarioController.js'
+import checkAuth from '../middleware/authMiddleware.js'
+
+// Area publica
+router.post('/', registrar)
+router.get('/confirmar/:token', confirmar)
+router.post('/login', autenticar)
+router.post('/olvide-password', olvidePassword)
+router.get('/olvide-password/:token', comprobarToken)
+router.post('/olvide-password/:token', nuevoPassword)
+
+
+// Area privada
+
+router.get('/perfil',checkAuth, perfil)
+
+export default router;
